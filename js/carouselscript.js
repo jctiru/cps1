25c3608946cf543cd9f50aed5250ffe57df08522
jctiru <jonathanchristiantiru@gmail.com>
2017-12-11 21:57:59 +0800

    Initial prototype

---
+++ js/carouselscript.js
@@ -0,0 +1,19 @@
+ $(document).ready(function () {
+    //initialize swiper when document ready
+   var galleryTop = new Swiper('.gallery-top', {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+      spaceBetween: 10,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      touchRatio: 0.2,
+      slideToClickedSlide: true,
+    });
+    galleryTop.controller.control = galleryThumbs;
+    galleryThumbs.controller.control = galleryTop;
+  });
