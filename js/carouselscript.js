af3ee6c57df6e940152277555966d8830bb0c100
jctiru <jonathanchristiantiru@gmail.com>
2017-12-12 13:32:49 +0800

    Added fade effect and autoplay to carousel

--- js/carouselscript.js
+++ js/carouselscript.js
@@ -1,19 +1,27 @@
- $(document).ready(function () {
-    //initialize swiper when document ready
-   var galleryTop = new Swiper('.gallery-top', {
-      spaceBetween: 10,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-      spaceBetween: 10,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      touchRatio: 0.2,
-      slideToClickedSlide: true,
-    });
-    galleryTop.controller.control = galleryThumbs;
-    galleryThumbs.controller.control = galleryTop;
-  });
+$(document).ready(function () {
+//initialize swiper when document ready
+var galleryTop = new Swiper('.gallery-top', {
+  lazy: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  effect: 'fade', 
+});
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  lazy: true,
+  spaceBetween: 10,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  touchRatio: 0.2,
+  slideToClickedSlide: true,
+});
+galleryTop.controller.control = galleryThumbs;
+galleryThumbs.controller.control = galleryTop;
+});
